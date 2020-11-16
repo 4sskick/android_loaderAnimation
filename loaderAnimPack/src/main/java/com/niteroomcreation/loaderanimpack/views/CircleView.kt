26de5d354af918e11dd93f84da8f17102138e901
Septian Adi Wijaya <developadi@gmail.com>
2020-11-16 20:29:32 +0700

    [circle] setup attributes - progress onDraw()

--- loaderAnimPack/src/main/java/com/niteroomcreation/loaderanimpack/views/CircleView.kt
+++ loaderAnimPack/src/main/java/com/niteroomcreation/loaderanimpack/views/CircleView.kt
@@ -2,6 +2,7 @@ package com.niteroomcreation.loaderanimpack.views
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 import com.niteroomcreation.loaderanimpack.R
@@ -11,37 +12,66 @@ import com.niteroomcreation.loaderanimpack.R
  */
 class CircleView : View {
 
-    constructor(context: Context?) : super(context)
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        initAttributes(attrs)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
         context,
         attrs,
         defStyleAttr
-    )
+    ) {
+        initAttributes(attrs)
+    }
 
-    constructor(
-        context: Context?,
-        attrs: AttributeSet?,
-        defStyleAttr: Int,
-        defStyleRes: Int
-    ) : super(context, attrs, defStyleAttr, defStyleRes)
+    //custom constructor
+    constructor(context: Context, circleRadius: Int, circleColor: Int) : super(context) {
+        this.circleRadius = circleRadius
+        this.circleColor = circleColor
+    }
 
+    constructor(context: Context, circleRadius: Int, circleColor: Int, drawOnlyStroke: Boolean, strokeWidth: Int) : super(context) {
+        this.circleRadius = circleRadius
+        this.circleColor = circleColor
+
+        this.circleDrawOnlyStroke = drawOnlyStroke
+        this.circleStrokeWidth = strokeWidth
+    }
+
+    //attrs declared on XML
     var circleRadius: Int = 30
-    var circleStrokeWidth:Int =0
-    var circleColor:Int = resources.getColor(android.R.color.holo_blue_bright)
+    var circleStrokeWidth: Int = 0
+    var circleColor: Int = resources.getColor(android.R.color.holo_blue_bright, null)
+    var circleDrawOnlyStroke: Boolean = false
+
+    private var paint: Paint = Paint()
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+
+
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+        val widthHeight = (2 * (circleRadius)) + circleStrokeWidth
+        setMeasuredDimension(widthHeight, widthHeight)
     }
 
     fun initAttributes(attrs: AttributeSet) {
 
         val typedArray = context.obtainStyledAttributes(attrs, R.styleable.CircleView, 0, 0)
 
+        this.circleRadius = typedArray.getDimensionPixelSize(R.styleable.CircleView_circleRadius, 30)
+        this.circleColor = typedArray.getColor(R.styleable.CircleView_circleColor, resources.getColor(android.R.color.holo_blue_bright, null))
+        this.circleDrawOnlyStroke = typedArray.getBoolean(R.styleable.CircleView_circleDrawOnlystroke, false)
+
+        if (this.circleDrawOnlyStroke)
+            this.circleStrokeWidth = typedArray.getDimensionPixelSize(R.styleable.CircleView_circleStrokeWidth, 0)
+
+        typedArray.recycle()
     }
 
 }
